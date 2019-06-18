7edc0389a56bd1b2f120db4e2862dea8ebe3fc13
kristen <kristen.chong.brown@gmail.com>
2019-06-17 22:31:20 -0600

    simple language filter
    re-processing comments to filter likely non-english text out. need to quit using csv, it's just the fastest for me to get working... T_T

--- scripts/process_comments.py
+++ scripts/process_comments.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 """ Script to process YouTube data """
 
+import os
 import time
 import sys
 import json
 import pandas as pd
 import numpy as np
+import fastText
 
 def get_metadata(infile):
     """ Opens a video JSON and returns relevant metadata """
@@ -78,9 +80,36 @@ def get_comments(infile):
             # Write whatever is left to a file
             comments.to_csv('comments_set_' + count + '.csv')
 
+def pred_lang(text, model):
+    """ Predict most likely language used"""
+    
+    return model.predict(text)[0][0].replace('__label__', '')
+
+def filter_lang(files, lang, model, directory):
+    """ Filter by predicted language """
+    
+    for f in files:
+        print(f)
+        try:
+            comments = pd.read_csv(directory+f, index_col=0)
+        except pd.errors.ParserError:
+            comments = pd.read_csv(directory+f, index_col=0, lineterminator='\n')
+        comments['text'] = comments['text'].apply(lambda x: str(x).replace('\n',' '))
+        comments['lang'] = comments['text'].apply(lambda x: pred_lang(x, model))
+        comments = comments[comments['lang'] == lang]
+        comments.to_csv(directory+'filtered/filtered_'+f)
+
 def main():
     """main routine"""
-    get_comments('youtube-dl-archive.txt')
+    # Get the comments and turn them into tables
+    #get_comments('youtube-dl-archive.txt')
+
+    # Filter non-english seeming comments
+    lang_mod = fastText.load_model('lid.176.bin')
+    directory = '/Users/kristenbrown/Documents/GitHub/youtube-data/label-app/data/'
+    filter_lang(os.listdir('/Users/kristenbrown/Documents/GitHub/youtube-data/label-app/data/'), 'en', lang_mod, directory)
+    
+    # Upload comments to SQL DB
 
 if __name__ == '__main__':
     main()
