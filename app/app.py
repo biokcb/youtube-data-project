6975a1abd100ebd873d4e26a918e8fd66f429c33
kristen <kristen.chong.brown@gmail.com>
2019-06-20 22:59:07 -0600

    revamp the webapp
    Major changes in the web app code - updated to utilize postgresql database, generate elements more dynamically, added a page to select tags, and overall changed some visuals

--- app/app.py
+++ app/app.py
@@ -1,58 +1,110 @@
-from flask import Flask, render_template, request
 import requests
 import pandas as pd
 import numpy as np
+import sqlalchemy
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy_utils import database_exists, create_database
+import psycopg2
+from nltk.corpus import stopwords
 
+## Functions required to run app
+def load_db(dbname, username):
+    """ Creates a connection to my PostgreSQL database """
+    engine = create_engine('postgres://%s@localhost/%s'%(username,dbname))
 
-#Initialize app
+    if not database_exists(engine.url):
+        create_database(engine.url)
+
+    connection = engine.connect()
+    
+    return engine, connection
+
+def get_top_videos(mood, engine, table):
+    """ Get the top ranking videos for a particular mood """
+    
+    # create a dataframe pulling all comment info from database for a video
+    videos = sqlalchemy.Table(table, sqlalchemy.MetaData(), autoload=True, autoload_with=engine)
+    
+    if mood == 'excited':
+        videos_query = sqlalchemy.select([videos]).order_by(videos.columns.excited_score.desc()).limit(20)
+    elif mood == 'relaxed':
+        videos_query = sqlalchemy.select([videos]).order_by(videos.columns.calm_score.desc()).limit(20)
+    elif mood == 'joking':
+        videos_query = sqlalchemy.select([videos]).order_by(videos.columns.joke_score.desc()).limit(20)
+    elif mood == 'annoyed':
+        videos_query = sqlalchemy.select([videos]).order_by(videos.columns.annoyed_score.desc()).limit(20)
+    else:
+        raise ValueError("Mood requested does not exist in database")
+        
+    videos_df = pd.read_sql(videos_query, engine)
+    
+    return videos_df
+
+def get_top_tags(df):
+    """ Grab the top 10 common video tags """
+    stop_words = set(stopwords.words('english') +
+                     ['buzzfeed', 'tasty', 'food'])
+    tags = df['top_tag'].dropna().str.lower().unique()
+    tags = [x for x in tags if x not in stop_words]
+
+    return tags[:10]
+
+def get_final_recs(df, tags):
+    """ After mood & tags are selected, return best recommendations """
+
+    if tags is None or tags == []:
+        df_tags = df[:3]
+    else:
+        df_tags = df[df['top_tag'].isin(tags)][:3]
+    
+    return df_tags
+    
+
+## Initialize app
 app = Flask(__name__, static_url_path='/static')
+engine, connection = load_db('cheftube', 'kristenbrown')
+
+## Render templates
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-@app.route('/recommendations', methods=['GET', 'POST'])
-def recommendations():
-    
-    if request.form['action'] == 'Relax':
-        channel_type = 'calm'
-    
-    elif request.form['action'] == 'Laugh':
-        channel_type = 'funny'
+@app.route('/recommended_tags', methods=['GET', 'POST'])
+def recommended_tags(engine=engine):
+    if 'action' in request.form.keys():
+        mood = request.form['action'].lower()
+        
+        df = get_top_videos(mood, engine, 'videos_model_3')
+        tags = get_top_tags(df)
 
-    elif request.form['action'] == 'Something else':
-        channel_type = 'upset'
+    return render_template('recommended_tags.html', tags=tags,
+                           mood=mood) 
 
-    channels = pd.read_csv('data/channels.csv')
-    channel_select = channels.loc[channels['label'] == channel_type]
+@app.route('/recommended_videos', methods=['GET', 'POST'])
+def recommended_videos():
+    if 'taglist' in request.form.keys():
+        tags = request.form.getlist('taglist')
+    else:
+        tags = None
 
-    channel1 = channel_select.iloc[0]['uploader']
-    channel2 = channel_select.iloc[1]['uploader']
-    channel3 = channel_select.iloc[2]['uploader']
-    
-    link1 = channel_select.iloc[0]['uploader_url']
-    link2 = channel_select.iloc[1]['uploader_url']
-    link3 = channel_select.iloc[2]['uploader_url']
+    if 'mood' in request.form.keys():
+        mood = request.form['mood']
+    else:
+        raise Exception('Mood not selected')
 
-    desc1 = channel_select.iloc[0]['webpage_url']
-    desc2 = channel_select.iloc[1]['webpage_url']
-    desc3 = channel_select.iloc[2]['webpage_url']
+    df = get_top_videos(mood, engine, 'videos_model_3')
+    df_final = get_final_recs(df, tags)
 
-    thumb1 = channel_select.iloc[0]['thumbnail']
-    thumb2 = channel_select.iloc[1]['thumbnail']
-    thumb3 = channel_select.iloc[2]['thumbnail']
+    vid_titles = df_final['title']
+    vid_urls = df_final['id']
+    vid_thumbs = df_final['thumbnail']
+    vid_descs = df_final['description']
 
-    title1 = channel_select.iloc[0]['title']
-    title2 = channel_select.iloc[1]['title']
-    title3 = channel_select.iloc[2]['title']
+    vid_data = zip(vid_titles,vid_urls,vid_thumbs,vid_descs)
 
-
-    return render_template('recommendations.html', 
-                            channel1 = channel1, channel2 = channel2, channel3 = channel3,
-                            link1 = link1, link2 = link2, link3 = link3,
-                            thumb1 = thumb1, thumb2=thumb2, thumb3=thumb3,
-                            title1=title1, title2=title2, title3=title3,
-                            desc1 = desc1, desc2=desc2, desc3=desc3)
+    return render_template('recommended_videos.html', vid_data=vid_data)
 
 if __name__ == '__main__':
     #this runs your app locally
